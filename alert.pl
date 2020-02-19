31dc1f12a7b017734b2d867ce84e7d3335fbaa5f
Dan Pelleg <dpelleg@yahoo-inc.com>
2020-02-19 22:28:03 +0200

    foo

--- alert.pl
+++ alert.pl
@@ -357,13 +357,16 @@ my @skip_sensors_missing = (         #                   Encode as station:senso
                                      'OP-HCU:.*', # 1/1/2020
                                      'BAZAN_HPU:.*', # 8/2/2020
                                      'BAZAN.VRU:.*', # 1/1/2020
-                                     'BAZAN.(HCU|HPU|FCC|HDSGO|VRU|VIS3|SRU3|CU3|CU4|B201B|B11|B21|B31|SRU4|ISO|TO4):.*', # 15/2/2020
-                                     'CAOL.(RTO|B1|B2):.*', # 15/2/2020
+                                     'CAOL.(RTO|B1|B2|B3):.*', # 15/2/2020
                                      'GADIV (RCO|PAREX):.*', # 15/2/2020
                                      'GADIV_OPER:.*', # 17/2/2020
                                      'GADIV-(TOL|AS2XYL1|PAREX):.*', # 17/2/2020
-                                     'GADIV.(AROM|PARX):.*', # 18/2/2020
                                      'TOREN:.*', # 18/2/2020
+                                     'Gadiv-(TOL|AS2XYL1|PAREX):.*', # 19/2/2020
+                                     'GADIV.(AROM|PARX|XYL):.*', # 18/2/2020
+                                     'BAZAN.(CCR|HCU|HPU|FCC|HDSGO|VRU|VIS3|SRU3|CU3|CU4|B201B|B11|B21|B31|SRU4|ISO|TO1|TO4|C120\(HDS\)):.*', # 15/2/2020
+                                     'HDS Hvgo down:.*', # 19/2/2020
+                                     'CAOL_New:.*', # 19/2/2020
     );
 
 my $skip_sensors_missing = '^(' . join("|", @skip_sensors_missing) . ')$';
